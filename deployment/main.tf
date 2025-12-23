1496b90f32c9d73f311a9317283c7f2b4bf8af57
Nick Anthony Miras <mirasnickanthony@gmail.com>
2025-12-23 18:02:21 +0800

    created spot instance request tf setup

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,11 +1,20 @@
-# Request a spot instance at $0.03
-resource "aws_spot_instance_request" "llm_trainer" {
+resource "aws_instance" "test_spot" {
   ami           = "ami-07b531d2a90722369"
-  instance_type = "g6e.xlarge"
-  spot_type     = "persistent"
-  instance_interruption_behavior = "stop"
+  instance_type = var.instance_type
+  key_name      = "llm-trainer"
 
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type = "persistent"
+    }
+  }
+
+  root_block_device {
+    volume_size = "150"
+  }
   tags = {
-    Name = "LLMTraining"
+    Name = "test-spot"
   }
 }
