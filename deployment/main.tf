1d54b9bf1a04ea169d8921e960eb36756267e876
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-01-16 20:42:53 +0800

    modified  for on-demand instance type and fixed setup script

--- deployment/main.tf
+++ deployment/main.tf
@@ -27,93 +27,63 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   ip_protocol       = "-1" # all ports
 }
 
-data "aws_ebs_volume" "skill_llm" {
-  most_recent = true
-  filter {
-    name   = "tag:Name"
-    values = ["skill-llm"]
-  }
-}
-
-resource "aws_instance" "test_spot" {
-  availability_zone = data.aws_ebs_volume.skill_llm.availability_zone
+resource "aws_instance" "llm_training_instance" {
   ami           = "ami-07b531d2a90722369"
   instance_type = var.instance_type
   key_name      = "skill-llm"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
-  instance_market_options {
-    market_type = "spot"
-    spot_options {
-      instance_interruption_behavior = "stop"
-      spot_instance_type = "persistent"
-    }
-  }
 
   root_block_device {
     volume_size = 100
     volume_type = "gp3"
   }
   tags = {
-    Name = "LLM Training Spot Instance"
+    Name = "LLM Training G6 Instance"
   }
 
   user_data = <<-EOF
       #!/bin/bash
+      WORKING_DIR="/opt/dlami/nvme"
 
       # Install necessary packages
       apt install python3-pip -y
-
-      # Create the mount directory
-      mkdir -p /home/ubuntu/dev
     
       # Wait for the device to be attached
       while [ ! -e /dev/nvme1n1 ]; do sleep 1; done
-      
-      # Create a file system on the volume (xfs is recommended for AWS)
-      # The '-f' forces the overwrite if the disk was previously formatted
-      mkfs -t xfs /dev/nvme1n1
 
-      # Mount the volume
-      mount /dev/nvme1n1 /home/ubuntu/dev
-
-      # Get the UUID of the device to ensure persistent mounting across reboots
-      UUID=$(blkid -s UUID -o value /dev/nvme1n1)
-
-      # Add entry to fstab for auto-mount on reboot
-      # echo "UUID=$UUID  /home/dev  xfs  defaults,nofail  0  2" >> /etc/fstab
-    
-      # Adjust permissions so the default user can access it (optional)
-      chown -R ubuntu:ubuntu /home/ubuntu/dev
-      chmod 755 /home/ubuntu/dev
-
-      cd /home/ubuntu/dev
-      ### Uncomment the lines below if fresh environment setup is needed ###
-      # git clone https://github.com/luvellieee/Thesis-2.git thesis
-      # chmod 744 thesis
+      cd $WORKING_DIR
+      git clone https://github.com/Nick-Miras/Skill-LLM-Cloud.git thesis
+      chown -R ubuntu:ubuntu thesis
+      chmod -R 744 thesis
       cd thesis
-      touch installation_log.txt
-      chown ubuntu:ubuntu installation_log.txt
-      chmod 644 installation_log.txt
+      chmod -R 755 src
 
-      ##############################
-      # Set up the virtual environment and install dependencies
-      ##############################
-      su ubuntu
-      ### Uncomment the lines below if fresh environment setup is needed ###
-      # mkdir .venv
-      # python3 -m venv .venv
-      # echo "" > installation_log.txt
-      # pip install -r Skill-Extraction/requirements.txt --log installation_log.txt
+      sudo -u ubuntu bash << 'EOU'
+
+          # Load the variables or set path again because we are in a new shell
+          WORKING_DIR="/opt/dlami/nvme"
+          cd "$WORKING_DIR/thesis"
+          
+          touch installation_log.txt
+          
+          # --- Fix for EXTERNALLY MANAGED ENVIRONMENT ---
+          # You must create the venv AND activate it within this block.
+          # When the venv is active, pip checks get bypassed.
+          
+          echo "Creating virtual environment..."
+          python3 -m venv .venv
+          
+          echo "Activating virtual environment..."
+          source .venv/bin/activate
+          
+          echo "Installing dependencies..."
+          # Now that venv is active, pip will install into .venv/lib 
+          # and not conflict with the system python.
+          pip install -r requirements.txt > installation_log.txt 2>&1
+      EOU
     EOF
 }
 
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = data.aws_ebs_volume.skill_llm.id
-  instance_id = aws_instance.test_spot.id
-}
-
 output "public_ip" {
-  value = aws_instance.test_spot.public_ip
+  value = aws_instance.llm_training_instance.public_ip
 }
