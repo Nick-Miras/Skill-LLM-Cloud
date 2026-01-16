6b282d09b12864b47739af8472f01e9ea859cdbc
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-01-16 12:54:12 +0800

    add postprocessing script for combining and evaluating prediction results

---
+++ src/postprocessing.sh
@@ -0,0 +1,3 @@
+python src/postprocessing/combine_prediction_results.py --base_file data/test.jsonl  --input_file data/test.json --prediction_file saves/Meta-Llama-3-8B-Instruct/lora/results/generated_predictions.jsonl --output_file saves/Meta-Llama-3-8B-Instruct/lora/results/processed_predictions.jsonl --tokenizer white_space
+
+python src/evaluation/evaluate_token_based_results.py --prediction_file saves/Meta-Llama-3-8B-Instruct/lora/results/processed_predictions.jsonl --label_file data/test.jsonl
